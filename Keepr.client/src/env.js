c7b475acc11829cfc8755744c855b9d76ac1ce7a
Dustin Bates <dustin.bates98@gmail.com>
2023-03-31 10:46:55 -0600

    initialize

---
+++ Keepr.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'codeworks-classroom.us.auth0.com'
+export const clientId = 'vag5QXyI2zuu0wjCwXSUUFPg0mzGVTNV'
+export const audience = 'https://codeworks-classroom.com'
